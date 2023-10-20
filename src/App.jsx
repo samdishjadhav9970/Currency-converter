ff59a90acf917fc6579abc0afa8b18da1fe9120a
samdish jadhav <samdishjadhav9970@gmail.com>
2023-10-20 10:59:06 +0530

    swap bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import useCurrencyInfo from './hooks/useCurrencyInfo'
 // import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
-  const [amount, setAmount] = useState()
+  const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState('usd')
   const [to, setTo] = useState('inr')
-  const [convertedAmount, setConvertedAmount] = useState()
+  const [convertedAmount, setConvertedAmount] = useState(0)
 
   const currencyInfo = useCurrencyInfo(from)
   const options = Object.keys(currencyInfo)
